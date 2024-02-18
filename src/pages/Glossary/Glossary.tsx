3d73a697d79a9c87c4c3eab7e045a335be0823f9
michac789 <michaelac978@gmail.com>
2024-02-18 20:33:55 +0800

    feat: simply glossary page

--- src/pages/Glossary/Glossary.tsx
+++ src/pages/Glossary/Glossary.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import { GlossaryContainer, TabContentContainer } from "./GlossaryStyle";
 import { MetaData } from "../../components/MetaData/MetaData";
 import { NavButton } from "../../components/NavButton/NavButton";
 import { Tabs } from "../../components/Tabs/Tabs";
 import { Text } from "../../components/Text/Text";
+import { artistGlossary, artworkGlossary } from "../../constants/glossary";
 import { metaData } from "../../constants/metadata";
+
+const sortedArtworkGlossary = [...artworkGlossary].sort((a, b) => a.name.localeCompare(b.name));
+const sortedArtistGlossary = [...artistGlossary].sort((a, b) => a.name.localeCompare(b.name));
 
 const Glossary = () => {
   const [selected, setSelected] = useState<number>(0);
@@ -13,24 +18,47 @@ const Glossary = () => {
   ];
 
   const tabContents = [
-    <div>Tab 1 Content</div>,
-    <div>Tab 2 Content</div>,
+    (
+      <TabContentContainer>
+        {sortedArtistGlossary.map((item, index) => (
+          <div key={index}>
+            <Text variant="body1" style={{ fontWeight: 500 }}>
+              {item.name}
+            </Text>
+            <Text variant="body2">
+              {item.description}
+            </Text>
+          </div>
+        ))}
+      </TabContentContainer>
+    ),
+    (
+      <TabContentContainer>
+        {sortedArtworkGlossary.map((item, index) => (
+          <div key={index}>
+            <Text variant="body1" style={{ fontWeight: 500 }}>
+              {item.name}
+            </Text>
+            <Text variant="body2">
+              {item.description}
+            </Text>
+          </div>
+        ))}
+      </TabContentContainer>
+    ),
   ];
   const handleTabClick = (index: number) => {
     setSelected(index);
   }
 
   return (
-    <>
+    <GlossaryContainer>
       <MetaData {...metaData['glossary']} />
-      <Text variant="title2">
-        Glossary - TODO
-      </Text>
       <Tabs tabs={tabs} selected={selected} onTabClick={handleTabClick} fullWidth>
         {tabContents[selected]}
       </Tabs>
       <NavButton />
-    </>
+    </GlossaryContainer>
   );
 }
 export default Glossary;
