9929c34c0bf948a572e104802216fe65b294a661
michac789 <michaelac978@gmail.com>
2024-03-03 18:31:42 +0800

    feat: useDebounce hook, simple input field for search modal

--- src/components/PageLayout/SearchModal.tsx
+++ src/components/PageLayout/SearchModal.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import { Backdrop } from "../Backdrop/Backdrop";
 import { Portal } from "../Portal/Portal";
 import { SearchModalContainer } from "./SearchModalStyle";
+import { useDebounce } from "../../utils/useDebounce";
 import { useOnClickOutside } from "../../utils/useOnClickOutside";
 
 interface SearchModalProps {
@@ -12,9 +13,12 @@ interface SearchModalProps {
 export const SearchModal = ({
   isOpen, onClose
 }: SearchModalProps) => {
+  const [searchValue, setSearchValue] = useState<string>("");
   const [isExiting, setIsExiting] = useState<boolean>(false);
   const modalRef = useRef<HTMLDivElement>(null);
   const timeout = useRef<NodeJS.Timeout | null>(null);
+
+  const debouncedSearchValue = useDebounce(searchValue);
 
   const handleClose = () => {
     setIsExiting(true);
@@ -33,12 +37,21 @@ export const SearchModal = ({
         onExit={handleClose}
       >
         <SearchModalContainer ref={modalRef}>
-          Remaining TODOs:
-          <ol>
-            <li>Improve search feature functionality and UI</li>
-            <li>Essay page allow image side by side or float wrap text (requested from NGS)</li>
-            <li>Consider migrating to gatsby for ssg</li>
-          </ol>
+          <div>
+            Remaining TODOs:
+            <ol>
+              <li>Improve search feature functionality and UI</li>
+              <li>Essay page allow image side by side or float wrap text (requested from NGS)</li>
+              <li>Consider migrating to gatsby for ssg</li>
+            </ol>
+          </div>
+          <div>
+            Search query:
+            <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+            <div>
+              Debounced value: {debouncedSearchValue}
+            </div>
+          </div>
         </SearchModalContainer>
       </Backdrop>
     </Portal>
