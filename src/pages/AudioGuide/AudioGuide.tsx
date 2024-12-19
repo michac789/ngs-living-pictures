06c4f1a5886ea60254dce3329a0eaeee4aaa0e60
michac789 <michaelac978@gmail.com>
2024-12-19 13:11:50 +0700

    fix: audio guide only allow one audio play at a time

--- src/pages/AudioGuide/AudioGuide.tsx
+++ src/pages/AudioGuide/AudioGuide.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AudioGuideContainer, AudioItemCard } from "./AudioGuideStyle";
 import { AudioPlayer } from "../../components/AudioPlayer/AudioPlayer";
 import { Markdown } from "../../components/Markdown/Markdown";
@@ -6,6 +6,12 @@ import { Text } from "../../components/Text/Text";
 import { Audio, audioList } from "../../constants/audio";
 
 const AudioGuide = () => {
+  const [currentAudio, setCurrentAudio] = useState<string | null>(null);
+
+  const handlePlay = (audioPath: string | null) => {
+    setCurrentAudio(audioPath);
+  };
+
   return (
     <AudioGuideContainer>
       <Text variant="title2">
@@ -19,7 +25,11 @@ const AudioGuide = () => {
           {audio.author && (
             <Text variant="body2">{audio.author}</Text>
           )}
-          <AudioPlayer audioPath={audio.audioPath} />
+          <AudioPlayer
+            audioPath={audio.audioPath}
+            isPlaying={currentAudio === audio.audioPath}
+            onPlay={handlePlay}
+          />
           {audio.descriptionMd && (
             <Markdown
               value={audio.descriptionMd}
